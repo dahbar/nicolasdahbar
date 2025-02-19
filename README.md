36238d19350e533c4fb86b2f003ebe3e582b4c6a
Nicolas Dahbar <dahbar42@gmail.com>
2025-02-19 22:10:23 +0100

    Initial Commit.

---
+++ README.md
@@ -0,0 +1,21 @@
+# Nicolas Dahbar - Personal Website
+
+This is my personal website built with Hugo using a modified version of the Sada theme. The site serves as my online resume and professional portfolio.
+
+## Technologies Used
+- Hugo
+- Tailwind CSS
+
+## Local Development
+To run the site locally:
+1. Install Hugo (extended version)
+2. Clone this repository
+3. Run `hugo server -D`
+
+## Build
+To build the site for production:
+1. Run `hugo`
+2. The built site will be in the `public` directory
+
+## Theme
+Based on the [Sada theme](https://github.com/darshanbaral/sada) by Darshan Baral, with custom modifications for dark mode and additional layouts.
---
+++ tailwind.config.js
@@ -0,0 +1,72 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  important: true,
+  content: [
+    "./layouts/**/*.html",
+    "./content/**/*.{html,md}",
+    "./themes/sada/layouts/**/*.html",
+    "./themes/sada/content/**/*.{html,md}",
+    "./assets/**/*.{js,css}",
+    // Include any other directories where Tailwind classes are used
+  ],
+  darkMode: "class", // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundColor: {
+        dark1: "#202124",
+        dark2: "#2a2b2e",
+        dark3: "#3c4043",
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            a: {
+              color: theme("colors.blue.700"),
+              textDecoration: "none",
+              "&:hover": {
+                color: theme("colors.blue.900"),
+                textDecoration: "underline",
+              },
+            },
+            p: {
+              marginBottom: "0em",
+              lineHeight: "1.5em",
+              marginTop: "0.25em",
+            },
+            h2: {
+              marginBottom: "1em",
+              marginTop: "0",
+            },
+          },
+        },
+        dark: {
+          css: {
+            color: theme("colors.gray.200"),
+            p: { color: theme("colors.gray.200") },
+            h1: { color: theme("colors.gray.200") },
+            h2: { color: theme("colors.gray.200") },
+            h3: { color: theme("colors.gray.200") },
+            h4: { color: theme("colors.gray.200") },
+            h5: { color: theme("colors.gray.200") },
+            h6: { color: theme("colors.gray.200") },
+            code: { color: theme("colors.gray.200") },
+            strong: { color: theme("colors.gray.200") },
+            td: { color: theme("colors.gray.200") },
+            blockquote: { color: theme("colors.gray.200") },
+            a: {
+              color: theme("colors.yellow.500"),
+              "&:hover": { color: theme("colors.yellow.300") },
+            },
+          },
+        },
+      }),
+    },
+  },
+  variants: {
+    extend: {
+      typography: ["dark"],
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
